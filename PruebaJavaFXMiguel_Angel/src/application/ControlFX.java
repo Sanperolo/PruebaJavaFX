99e35fd0dd81e0f8f690ee50380474f4e3000032
Sanperolo <michaelherranz@gmail.com>
2017-12-01 09:50:45 +0100

    Start the exam

---
+++ PruebaJavaFXMiguel_Angel/src/application/ControlFX.java
@@ -0,0 +1,51 @@
+package application;
+
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
+
+public class ControlFX {
+	
+    @FXML
+    private Button btnCerrar;
+	
+   @FXML
+   void closeApp(MouseEvent event) {
+   	System.exit(0);
+   }
+   
+   @FXML
+   void cerrarSegundaVentana(MouseEvent event ) {
+	   
+	   Stage thisStage = (Stage)  btnCerrar.getScene().getWindow();
+	   thisStage.close();
+
+	   }
+     
+
+   public void openStage() {
+       try {
+           FXMLLoader loader = new FXMLLoader(MainFX.class.getResource("../views/FreeSolo.fxml"));
+           AnchorPane ventanaDos = (AnchorPane) loader.load();
+           Stage ventana = new Stage();
+           ventana.setTitle("Open FreeSolo");
+           Window stagePrincipal = null;
+		   ventana.initOwner(stagePrincipal);
+           Scene scene = new Scene(ventanaDos);
+           ventana.setScene(scene);
+           ventana.show();
+
+       } catch (Exception e) {
+           //tratar la excepción
+       }
+   }
+
+ 
+   
+}
---
+++ PruebaJavaFXMiguel_Angel/src/application/MainFX.java
@@ -0,0 +1,43 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.ImageCursor;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+
+public class MainFX extends Application {
+	
+private AnchorPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+		
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainFX.class.getResource("../views/Login.fxml"));
+			rootLayout = (AnchorPane) loader.load();
+						
+						
+			// BorderPane root = new BorderPane();
+			Scene scene = new Scene(rootLayout,500,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
